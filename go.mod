0bdacc1901d9f620e51481d67e6ade9aa15ee8bc
Ben Meehan <benmeehan111@example.com>
2023-10-22 14:33:16 +0530

    added kafka

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/tic-tac-toc-edge/matchGameService
+
+go 1.20
+
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.2.0
+	github.com/eclipse/paho.mqtt.golang v1.4.3
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+const (
+	mqttBrokerURL = "tls://s341caa4.ala.us-east-1.emqxsl.com:8883"
+	mqttClientID  = "game_processor"
+	mqttTopic     = "play_game"
+	kafkaBroker   = "localhost:9092"
+	kafkaTopic    = "game_topic"
+)
+
+var kafkaProducer *kafka.Producer
+
+func onMQTTMessageReceived(client MQTT.Client, msg MQTT.Message) {
+	id := string(msg.Payload())
+	fmt.Printf("Received ID: %s\n")
+
+	deliveryChan := make(chan kafka.Event)
+
+	message := &kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: kafkaTopic, Partition: kafka.PartitionAny},
+		Value:          []byte(id),
+		Headers:        []kafka.Header{{Key: "expiration-time", Value: []byte(fmt.Sprintf("%d", expirationTime.UnixNano()/int64(time.Millisecond)))}},
+	}
+	kafkaProducer.Produce(message, deliveryChan)
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		log.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+	} else {
+		log.Printf("Delivered message to topic %s [%d] at offset %v\n", *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+	}
+}
+
+func main() {
+	opts := MQTT.NewClientOptions().AddBroker(mqttBrokerURL).SetClientID(mqttClientID).SetUsername("ben").SetPassword("bm12")
+	client := MQTT.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to connect to MQTT broker: %v", token.Error())
+		os.Exit(1)
+	}
+
+	if token := client.Subscribe(mqttTopic, 0, onMQTTMessageReceived); token.Wait() && token.Error() != nil {
+		log.Fatalf("Failed to subscribe to MQTT topic: %v", token.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Listening for messages on MQTT topic 'play_game'...")
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaBroker, "enable.idempotence": true})
+	if err != nil {
+		log.Fatalf("Failed to create Kafka producer: %v", err)
+		os.Exit(1)
+	}
+	kafkaProducer = p
+
+	defer kafkaProducer.Close()
+
+	select {}
+}
